e991d48820e9e37c5c6aee73348c127d195b4765
Trương Tam Lang <lang.tt16@gmail.com>
2017-12-14 15:20:28 +0700

    new sql + update course
    new sql
    update course
    transaction

--- TMDT_REST_V2/src/main/java/com/spring/repository/CourseRepository.java
+++ TMDT_REST_V2/src/main/java/com/spring/repository/CourseRepository.java
@@ -7,11 +7,18 @@ import com.spring.domain.Course;
 
 public interface CourseRepository {
 	public Optional<Course> getCourseByCourseID(String courseID);
-	
-	public Map<String, Object> getCourseWithPaging(int page,int size);
-	
-	public Map<String, Object>getCourseByTopicIDWithPaging(int page,int size,String TopicID);
-	
-	public int updateCourseStatus(String courseID,int newStatus);
-	
+
+	public Map<String, Object> getCourseWithPaging(int page, int size);
+
+	public Map<String, Object> getCourseByTopicIDWithPaging(int page, int size, String TopicID);
+
+	public int updateCourseStatus(String courseID, int newStatus);
+
+	public String createCourse(String courseTitle, String courseDescription, String author, Integer price,
+			String courseTypeID, String topicID, String courseAvatar, String courseDetail);
+
+	public int updateCourse(String courseID, String courseTitle, String courseDescription, Integer price,
+			String courseTypeID, String topicID, String courseAvatar, String courseDetail, Integer newStatus);
+
+
 }
--- TMDT_REST_V2/src/main/java/com/spring/service/CourseService.java
+++ TMDT_REST_V2/src/main/java/com/spring/service/CourseService.java
@@ -7,36 +7,74 @@ import com.spring.domain.Course;
 
 public interface CourseService {
 	public Optional<Course> getCourseByCourseID(String courseID);
-	
+
 	/**
-	 * get topic with paging parameter 
-	 * @param page current page begin with 1
-	 * @param size number of record per page
+	 * get topic with paging parameter
+	 * 
+	 * @param page
+	 *            current page begin with 1
+	 * @param size
+	 *            number of record per page
 	 * @return Map<String, Object> with two key (listOfResult, numberOfPage)
 	 */
-	public Map<String, Object> getCourseWithPaging(int page,int size);
-	
+	public Map<String, Object> getCourseWithPaging(int page, int size);
+
 	/**
 	 * get course by topic id with paging
+	 * 
 	 * @param page
 	 * @param size
-	 * @param TopicID 
-	 * @return  Map<String, Object> with two key (listOfResult, numberOfPage)
+	 * @param TopicID
+	 * @return Map<String, Object> with two key (listOfResult, numberOfPage)
 	 */
-	public Map<String, Object>getCourseByTopicIDWithPaging(int page,int size,String TopicID);
-	
+	public Map<String, Object> getCourseByTopicIDWithPaging(int page, int size, String TopicID);
+
 	/**
 	 * update course status
+	 * 
 	 * @param courseID
 	 * @param newStatut
 	 * @return
 	 */
-	public int updateCourseStatut(String courseID,int newStatut);
+	public int updateCourseStatut(String courseID, int newStatut);
+
 	/**
 	 * check user is author of course
+	 * 
 	 * @param accountID
 	 * @param courseID
 	 * @return true if is author
 	 */
-	public boolean isCourseAuthor(String accountID,String courseID);
+	public boolean isCourseAuthor(String accountID, String courseID);
+
+	/**
+	 * create course
+	 * @param courseTitle
+	 * @param courseDescription
+	 * @param author (userID)
+	 * @param price
+	 * @param courseTypeID
+	 * @param topicID
+	 * @param courseAvatar
+	 * @param courseDetail
+	 * @return CourseID (null if create fail)
+	 */
+	public String createCourse(String courseTitle, String courseDescription, String author, Integer price,
+			String courseTypeID, String topicID, String courseAvatar, String courseDetail);
+
+	/**
+	 * update course
+	 * @param courseID
+	 * @param courseTitle
+	 * @param courseDescription
+	 * @param price
+	 * @param courseTypeID
+	 * @param topicID
+	 * @param courseAvatar
+	 * @param courseDetail
+	 * @param newStatus
+	 * @return  The number of rows affected by the update
+	 */
+	public int updateCourse(String courseID, String courseTitle, String courseDescription, Integer price,
+			String courseTypeID, String topicID, String courseAvatar, String courseDetail, Integer newStatus);
 }
