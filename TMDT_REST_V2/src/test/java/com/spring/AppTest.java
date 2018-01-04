33d739c883edd4c6d53865aa40b038dd2824a229
TrinhPham96 <nhoccodaihp96@gmail.com>
2018-01-04 12:40:30 +0700

    Revert "transaction_history"
    This reverts commit 54e18248a8a94b8b9ef7de0407697e608a2d7a39.

--- TMDT_REST_V2/src/test/java/com/spring/AppTest.java
+++ TMDT_REST_V2/src/test/java/com/spring/AppTest.java
@@ -7,18 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.spring.service.ChapterService;
-import com.spring.service.UserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppTest {
 	@Autowired
 	ChapterService chapterService;
-	UserService userService;
 	@Test
 	public void testA() {
 		System.err.println(chapterService.updateChapter(null, "hihi","hihi", "KH6CH12"));
 	}
-	
-	
 }
