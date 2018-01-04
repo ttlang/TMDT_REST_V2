21e9ce2fb50069c475eaa8887f4c73c7a03d8cfa
vanthang1996 <vanthang20437@gmail.com>
2018-01-04 15:48:53 +0700

    Update AppTest.java

--- TMDT_REST_V2/src/test/java/com/spring/AppTest.java
+++ TMDT_REST_V2/src/test/java/com/spring/AppTest.java
@@ -5,21 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-<<<<<<< HEAD
 import com.spring.service.ChapterService;
 import com.spring.service.CourseService;
 import com.spring.service.LessonService;
 import com.spring.service.PermissionCheck;
-=======
 import com.spring.service.LessonAttachService;
->>>>>>> master
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppTest {
 	@Autowired
-<<<<<<< HEAD
 	ChapterService chapterService;
 	@Autowired
 	CourseService courseService;
@@ -28,10 +23,7 @@ public class AppTest {
 	@Autowired
 	LessonService lessonService ;
 
-=======
-	LessonAttachService lessonAttachService;
 	
->>>>>>> master
 	@Test
 	public void testA() {
 	
