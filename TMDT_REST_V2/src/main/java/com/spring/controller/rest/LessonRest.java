ebde57bbc9ea898d9ab8f61be8ec0cc0edeb90d0
vanthang1996 <vanthang20437@gmail.com>
2018-01-04 15:42:59 +0700

    Update LessonRest.java

--- TMDT_REST_V2/src/main/java/com/spring/controller/rest/LessonRest.java
+++ TMDT_REST_V2/src/main/java/com/spring/controller/rest/LessonRest.java
@@ -4,11 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-<<<<<<< HEAD
-import javax.servlet.http.HttpServletRequest;
-=======
->>>>>>> master
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
