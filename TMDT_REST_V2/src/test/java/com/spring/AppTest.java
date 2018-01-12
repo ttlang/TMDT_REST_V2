e09e111deeee82687476f1764997ac3b907c7d07
TrinhPham96 <nhoccodaihp96@gmail.com>
2018-01-12 16:44:52 +0700

    add tmdt_v011.sql

--- TMDT_REST_V2/src/test/java/com/spring/AppTest.java
+++ TMDT_REST_V2/src/test/java/com/spring/AppTest.java
@@ -1,19 +1,11 @@
 package com.spring;
 
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 
 import com.spring.service.CourseService;
 import com.spring.service.GoogleDriveApiService;
@@ -33,13 +25,6 @@ public class AppTest {
 			System.out.println(k + " : " + v);
 		});
 
-	}
-	
-	@Test
-	public void testSearchCouseByName() {
-		Map<String, Object> result = this.courseService.searchByCourseName(1, 2, "android");
-		List<Course> listCourseResult = (List<Course>) result.get("listOfResult");
-		assertEquals(2, listCourseResult.size());
 	}
 
 }
